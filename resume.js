5366b5d5b8ea711dfb8adfe09e5e0d4190c26009
Krithik Rao <krdevmail@gmail.com>
2019-06-29 16:26:39 -0400

    ADDED my own resume with basic content
    resume.js defines my resume in pure json and resume.toml does so in
    toml.
    
    Currently resume.js is what actually gets used. Resume.toml exists for
    now simply because it's easier to edit. Eventually we'd like to switch
    to the purely TOML version without including too many dependencies.

---
+++ resume.js
@@ -0,0 +1,70 @@
+export const resume = {
+  personal: {
+    name: "Krithik Rao",
+    contact: [
+      "309-826-4765",
+      "krdevmail@gmail.com",
+      "linkedin.com/in/krithikr",
+      "github.com/KritR"
+    ],
+  },
+  blocks: [
+    {
+      title: "Education",
+      subheadings: [
+        {
+          title: "Purdue University - 3.84 GPA",
+          duration: "May 2018 - Dec 2020",
+          notes: [
+            "Bachelor's In Computer Science • Machine Learning Concentration"
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Experience",
+      subheadings: [
+        {
+          title: "GE Transportation/Wabtec - Software Engineering Intern",
+          duration: "May 2019 - Aug 2019",
+          notes: [
+            "Developed automated test infrastructure for the Remote Control Locomotive product.",
+            "Lead package manager integration initiative to ease software deployment procedure.",
+            "Deployed a command line parser library in C#.",
+            "Designed graphics for intern hosted professional development and fundraising events.",
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Activities",
+      subheadings: [
+        {
+          title: "FIRST Robotics | Team 4213 - Software Team Lead",
+          duration: "Aug 2015 - Sep 2017",
+          notes: [
+            "Implemented a custom scheduler and state machine for robot operations and vision processing.",
+            "Architected a Laravel web application for a Sponsor Relationship Management system.",
+            "Lead CAD and programming workshops for new teammates.",
+          ]
+        }
+      ]
+    },
+
+    {
+      title: "Achievements",
+      subheadings: [
+        {
+          notes: [
+            "Won 3rd in Computer Science at Worldwide Youth Science and Engineering | State — 2018",
+            "Placed 4th at the National Leadership Conference in Computer Networking — Future Business Leaders of America — 2016",
+            "Oracle Certified Associate Java SE Programmer — 2015",
+          ]
+        }
+      ]
+    }
+  ]
+
+};
