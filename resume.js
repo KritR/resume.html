37b74a6ae49b3fdc84942e69e8a03c6499cf4c32
Krithik Rao <krdevmail@gmail.com>
2019-07-11 09:34:04 -0400

    Minor Wording Updates and Reposition of Content
    Changed up the wording on some specific items.
    Like Bachelor of Science Computer Science.
    Also moved skills up before activities.

--- resume.js
+++ resume.js
@@ -17,7 +17,7 @@ export const resume = {
           title: "Purdue University - 3.84 GPA",
           duration: "May 2018 - Dec 2020",
           notes: [
-            "Bachelor's In Computer Science — Machine Learning Concentration — Math Minor"
+            "Bachelor of Science Computer Science — Machine Learning Concentration — Math Minor"
           ]
         }
       ]
@@ -27,19 +27,19 @@ export const resume = {
       title: "Experience",
       subheadings: [
         {
-          title: "GE Transportation/Wabtec - Software Engineering Intern",
-          duration: "May 2019 - Aug 2019",
+          title: "GE Transportation - Software Engineering Intern",
+          duration: "May 2019 - Jul 2019",
           notes: [
             "Developed automated test infrastructure for the Remote Control Locomotive product.",
-            "Lead package manager integration initiative to ease software deployment procedure.",
-            "Deployed a command line parser library in C#.",
+            "Led package manager integration initiative to ease software deployment procedure.",
+            "Deployed a command line argument parser library in C# with unit tests and example usage.",
           ]
         },
         {
           title: "Freelancing - Software Developer",
           duration: "Jan 2015 - May 2019",
           notes: [
-            "HuttonPatt — Designed and deployed website in Django based CMS.",
+            "HuttonPatt — Designed and deployed website in Django based content management system.",
             "Bountify.co — Developed C#/Win32 app to block keyboard input and used Ruby to query RESTful API.",
           ]
         }
@@ -50,42 +50,13 @@ export const resume = {
       subheadings: [
         {
           notes: [
-            "MarkovAndMe — Used NLTK to tokenize texts and designed a markov model to generate new ones.",
-            "FDS Purdue — MithrilJS based activity point tracker with NodeJS backend."
+            "MarkovAndMe — Used Natural Language Toolkit and designed Markov model for text generation.",
+            "FDS Purdue — Gamified activity tracker with scoreboard with NodeJS backend and MithrilJS frontend."
           ]
         }
       ]
     },
 
-    {
-      title: "Activities",
-      subheadings: [
-        {
-          title: "Hackathons",
-          duration: "Aug 2018 - Present",
-          notes: [
-            "Hello World @ Purdue — Banana Skateboard — Mobile/Desktop platformer game made with Lua/Love2d.",
-            "HackXD @ Purdue — Purdue Fit — Alexa Skill to query dining court meals and rec-center occupancy.",
-            "Code for Good @ JPMorgan — Mindful — Android app for individual activity tracking and suggestion."
-          ]
-        },
-        {
-          title: "FIRST Robotics | Team 4213 - Software Team Lead",
-          duration: "Aug 2015 - Sep 2017",
-          notes: [
-            "Implemented a custom scheduler and state machine for robot operations and vision processing.",
-            "Architected a Laravel web application for a Sponsor Relationship Management system.",
-            "Lead CAD and programming workshops for new teammates.",
-          ]
-        },
-        {
-          title: "Miscellaneous",
-          notes: [
-            "ACM SIG AI - President, b01lers (CTF Team), All Campus Community Choir, Machine Learning and Performing Arts research, Hack Club @ High School"
-          ]
-        }
-      ]
-    },
     {
       title: "Skills",
       subheadings: [
@@ -101,11 +72,41 @@ export const resume = {
     },
 
     {
+      title: "Activities",
+      subheadings: [
+        {
+          title: "Hackathons",
+          duration: "Aug 2018 - Present",
+          notes: [
+            "Hello World @ Purdue — Banana Skateboard — Mobile/Desktop platformer game made with Lua/Love2d.",
+            "HackXD @ Purdue — Purdue Fit — Amazon Alexa app to query dining court meals and gym occupancy.",
+            "Code for Good @ JPMorgan — Mindful — Android app for individual activity tracking and suggestion."
+          ]
+        },
+        {
+          title: "FIRST Robotics | Team 4213 - Software Team Lead",
+          duration: "Aug 2015 - Sep 2017",
+          notes: [
+            "Implemented a custom scheduler and state machine for robot operations and vision processing.",
+            "Architected a Laravel web application for a Sponsor Relationship Management system.",
+            "Led CAD and programming workshops for new teammates.",
+          ]
+        },
+        {
+          title: "Miscellaneous",
+          notes: [
+            "ACM SIG AI - President, b01lers (CTF Team), All Campus Community Choir, Machine Learning and Performing Arts research, Hack Club @ High School"
+          ]
+        }
+      ]
+    },
+
+    {
       title: "Achievements",
       subheadings: [
         {
           notes: [
-            "Won 3rd in Computer Science at State — Worldwide Youth Science and Engineering — 2018",
+            "Ranked 3rd in Computer Science at State — Worldwide Youth Science and Engineering — 2018",
             "Placed 4th at National's in Computer Networking — Future Business Leaders of America — 2016",
             "Oracle Certified Associate Java SE Programmer — 2015",
           ]
